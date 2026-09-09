918587a50b60b14763569e2993bae42851822a3e
Asti Sojitra <astisojitra@gmail.com>
2026-09-09 17:18:50 +0530

    Student Management System

--- StudentManagement.py
+++ StudentManagement.py
@@ -1,21 +1,79 @@
-students=[]
+students = []
+
 while True:
-    print("1. Add student")
-    print("2. View students")
-    print("3. Exit")
-    
-    choice = input("Enter your choice: ")
-    if choice == '1':
-        name =input("Enter student name:")
-        age=input("Enter student age:")
-        course=input("Enter student course:")
-        students.append({'name':name, 'age':age, 'course':course})
-    elif choice == '2':
-        print("-------- Student List --------")
+
+    print("-----------------student management system-------------------")
+    print("Select an option:")
+    print("1.Add student:")
+    print("2.Display all student:")
+    print("3.Search student")
+    print("4.Update student")
+    print("5.Delete student")
+    print("6.Display subject offered")
+    print("7.Exit:")
+
+    choice = input("Enter your choice (1-7): ")
+
+    if choice == "1":
+
+        name = input("Enter student name:")
+        age =int(input("Enter student age:"))
+        course = input("Enter student course:")
+        student ={"name": name, "age": age, "course": course}
+        students.append(student)
+        print("Student added successfully!!!")
+
+    elif choice == "2":
+
+        print("-----------------Student List-------------------")
         for student in students:
-            print(f"name: {student['name']}, age: {student['age']}, course: {student['course']}")
-    elif choice == '3':
-        print("exiting...")
+            print(student)
+
+    elif choice == "3":
+
+        name = input("Enter student name:")
+        for student in students:
+            if student['name'] == name:
+                print(student)
+                break
+        else:
+            print("student not found??")
+    elif choice == "4":
+        
+        print("---------------Update student-------------------")
+        name = input("Enter student name:")
+        for student in students:
+            if student['name'] == name:
+                student["name"] = input("Enter new name:")
+                student["age"] = int(input("Enter new age:"))
+                student["course"] = input("Enter new course:")
+                print("Student updated successfully!!!")
+                break
+        else:
+            print("student not found??")
+
+    elif choice == "5":
+
+        print("---------------Delete student-------------------")
+        name = input("Enter student name:")
+        for student in students:
+            if student['name'] == name:
+                students.remove(student)
+                print("Student deleted successfully!!!")
+                break
+        else:
+            print("student not found??")
+
+    elif choice == "6":
+        print("\n--------Subject Name--------")
+        print("Python")
+        print("C++")
+        print("PHP")
+
+    elif choice == "7":
+
+        print("Thank you for using the student management system!")
         break
-else:
-    print("invalid choice. please try again.")
+    
+    else:
+        print("Invalid choice....")
